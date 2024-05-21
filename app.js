fa29bf59d68892154606e61bc9b16641fd59c867
Shahzad090 <153703186+Shahzad090@users.noreply.github.com>
2024-05-21 03:19:59 +0100

    ..

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 function getNumber(num) {
   var result = document.getElementById("result");
-  
+
   result.value += num;
+}
+
+function clearResult() {
+    var result = document.getElementById("result")
+
+    result.value += ""
 }
