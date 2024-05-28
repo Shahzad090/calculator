efffd3e20e876f6dcae0a8ea9ffababfbadd7154
Shahzad090 <153703186+Shahzad090@users.noreply.github.com>
2024-05-28 01:07:21 +0100

    calculator is ready

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-function getNumber(num){
+function getNumber(num) {
   var result = document.getElementById("result");
 
   result.value += num;
 }
 
-function clearResult(){
-    var result = document.getElementById("result")
-    result.value =  ""
+function clearResult() {
+  var result = document.getElementById("result");
+  result.value = "";
 }
 
-// function getResult(){
-//   var result = document.getElementsByid("result")
-//     console.log(result.value)
-// }
+function getResult() {
+  var result = document.getElementById("result");
+  result.value = eval(result.value)
+}
